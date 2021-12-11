002252e9273ffe17ab48faed428e28fe7227b3bb
Luís Azevedo <luismiguelgomes2001@gmail.com>
2021-12-11 15:50:07 -0100

    loader interface 1.0v

--- src/components/LoadingDisplayer/LoadingDisplayer.jsx
+++ src/components/LoadingDisplayer/LoadingDisplayer.jsx
@@ -1,5 +1,15 @@
+import "./loader.style.scss";
+
 const LoadingDisplayer = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="loader_wrapper">
+      <div className="loader">
+        <div className="inner one"></div>
+        <div className="inner two"></div>
+        <div className="inner three"></div>
+      </div>
+    </div>
+  );
 };
 
 export default LoadingDisplayer;
