44cc39f9ddee7a50c86416c02d7a9f148e5f86aa
Luís Azevedo <luismiguelgomes2001@gmail.com>
2021-12-20 21:55:12 -0100

    NEW: ErrorBoundary support for handling errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { Fragment, useEffect, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "./components/FallBackErrorBoundary/FallBackErrorBoundary";
 import LoadingDisplayer from "./components/LoadingDisplayer/LoadingDisplayer";
+import { useErrorHandler } from "react-error-boundary";
 
 export default function App() {
   const [isAppLoading, setIsLoading] = useState(false);
+
+  const errorHanldler = (error, errorInfo) => {
+    console.log("Error", error, errorInfo);
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -13,14 +20,35 @@ export default function App() {
 
   return (
     <Fragment>
-      {isAppLoading ? (
-        <LoadingDisplayer />
-      ) : (
-        <div>
-          <h1>App</h1>
-          <p>Lorem ipsum</p>
-        </div>
-      )}
+      <ErrorBoundary FallbackComponent={ErrorFallback} onError={errorHanldler}>
+        {isAppLoading ? (
+          <LoadingDisplayer />
+        ) : (
+          <div>
+            <h1>App</h1>
+            <p>Lorem ipsum</p>
+            <ErrorThrower />
+          </div>
+        )}
+      </ErrorBoundary>
     </Fragment>
   );
 }
+
+const ErrorThrower = () => {
+  const errorHanldler = useErrorHandler();
+
+  const setError = () => {
+    try {
+      throw new Error("Error Test");
+    } catch (e) {
+      errorHanldler(e);
+    }
+  };
+
+  return (
+    <Fragment>
+      <button onClick={setError}>Get Error</button>
+    </Fragment>
+  );
+};
---
+++ src/components/FallBackErrorBoundary/FallBackErrorBoundary.jsx
@@ -0,0 +1,8 @@
+export function ErrorFallback({ error }) {
+  return (
+    <div role="alert">
+      <h2>Something went wrong:</h2>
+      <p>{error.message}</p>
+    </div>
+  );
+}
