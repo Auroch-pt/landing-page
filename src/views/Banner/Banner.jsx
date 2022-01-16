5427190315e70d47af0f1ec68f7c5b633a94720c
Beatriz <beatrizalmeidapires@hotmail.com>
2022-01-16 17:46:15 +0000

    Added banner translations

--- src/views/Banner/Banner.jsx
+++ src/views/Banner/Banner.jsx
@@ -1,43 +1,35 @@
 import "./Banner.styles.scss";
+import { useState } from "react";
 import developers from "../../resources/developers.svg";
 import { HiCheck } from "react-icons/hi";
+import { t } from "i18next";
 
 const Banner = () => {
+  const [benefits, setBenefits] = useState([
+    t("banner.card1"),
+    t("banner.card2"),
+    t("banner.card3"),
+  ]);
+
   return (
     <div className="banner__container">
       <div className="banner">
         <div className="banner__text">
-          <h2>You decide when and where to work</h2>
-          <h4>
-            Freedev is a platform where you can build projects, either by solving tasks or by
-            creating them.
-          </h4>
-          <button>Get Started</button>
+          <h2>{t("banner.title")}</h2>
+          <h4>{t("banner.description")}</h4>
+          <button>{t("header.enroll")}</button>
         </div>
         <img alt="" src={developers} />
       </div>
       <div className="banner__rectangles">
-        <div className="banner__rectangle">
-          <HiCheck size={60} color="F78317" />
-          <h6>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </h6>
-        </div>
-        <div className="banner__rectangle">
-          <HiCheck size={60} color="F78317" />
-          <h6>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </h6>
-        </div>
-        <div className="banner__rectangle">
-          <HiCheck size={60} color="F78317" />
-          <h6>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </h6>
-        </div>
+        {benefits.map((benefit) => {
+          return (
+            <div className="banner__rectangle">
+              <HiCheck size={60} color="F78317" />
+              <h6>{benefit}</h6>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
