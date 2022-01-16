1c476e638d1f359145fce4540991cb231469dcf0
beatrizalmeidapires <56452671+beatrizalmeidapires@users.noreply.github.com>
2022-01-16 18:27:22 +0000

    Revert "Cta banner"

--- src/components/Translator/i18n.js
+++ src/components/Translator/i18n.js
@@ -24,28 +24,7 @@ i18n
             description: "Freedev is the future of freelancing",
           },
           header: {
-            nav_option1: "Home",
-            nav_option2: "FAQ's",
-            nav_option3: "About Us",
             enroll: "Get started",
-          },
-          banner: {
-            title: "You decide when and where to work",
-            description:
-              "Freedev is a platform where you can build projects, either by solving tasks or by creating them.",
-            card1:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-            card2:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-            card3:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-          },
-          CTABanner: {
-            title: "Let's start building now",
-            subtitle: "Connect with your wallet and start solving issues.",
-          },
-          footer: {
-            info: "© 2022 Freedev. All rights reserved.",
           },
           error: {
             message: "Default Error",
@@ -59,28 +38,7 @@ i18n
             description: "Freedev é o futuro do freelancing",
           },
           header: {
-            nav_option1: "Home",
-            nav_option2: "FAQ's",
-            nav_option3: "Sobre Nós",
-            enroll: "Começar",
-          },
-          banner: {
-            title: "Tu decides quando e onde trabalhar",
-            description:
-              "Freedev é uma plataforma onde podes construir projetos, a resolver tarefas ou a criá-las",
-            card1:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-            card2:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-            card3:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-          },
-          CTABanner: {
-            title: "Vamos começar a desenvolver projectos",
-            subtitle: "Conecta-te com a tua wallet e começa agora a desenvolver projetos.",
-          },
-          footer: {
-            info: "© 2022 Freedev. Todos os direitos reservados.",
+            enroll: "Começar Agora",
           },
           error: {
             message: "Erro teste",
