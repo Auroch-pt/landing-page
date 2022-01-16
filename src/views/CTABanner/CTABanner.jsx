417e3eba35940267ebb7dc2a51f1dfda7e6d8e61
Beatriz <beatrizalmeidapires@hotmail.com>
2022-01-16 18:38:39 +0000

    Changed classname

--- src/views/CTABanner/CTABanner.jsx
+++ src/views/CTABanner/CTABanner.jsx
@@ -3,7 +3,7 @@ import { t } from "i18next";
 
 const CTABanner = () => {
   return (
-    <div className="wrapper">
+    <div className="cta-banner">
       <h2>{t("CTABanner.title")}</h2>
       <h4>{t("CTABanner.subtitle")}</h4>
       <button>{t("header.enroll")}</button>
